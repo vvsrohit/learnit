d28e3f72a81ed75c73fdcf74b1d8b61af000fd92
vvs rohit kumar <vvsrohit1112@gmail.com>
2026-01-13 09:48:12 +0530

    [m1]

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ console.log(`You are ${age} years old.`);
 function isabove18(age) {
     return age > 18;
 }
+console.log(`Is above 18: ${isabove18(age)}`);
+const hobbies = ["reading", "gaming", "coding"];
+console.log(`Hobbies: ${hobbies.join(", ")}`);
+
